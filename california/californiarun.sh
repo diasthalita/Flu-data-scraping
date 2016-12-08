be4adb4fd522b5ac205999870bc5e349e7b26887
diasthalita <dias.t@husky.neu.edu>
2016-12-08 13:29:43 -0500

    Update californiarun.sh

--- california/californiarun.sh
+++ california/californiarun.sh
@@ -2,9 +2,18 @@
 # File:         californiarun.sh 
 # Date:         Thu Dec  8 10:05:25 EST 2016
 # Author(s):    Thalita Coleman  <thalitaneu@gmail.com>
-# Abstract:		 Executes programs california.py and parsecalifornia.py. Parses the file cali1.htm  -- which contains the HTML source for the page https://www.cdph.ca.gov/data/statistics/Pages/CISPDataArchive.aspx -- and returns the URL of all weekly flu reports. Downloads the flu reports in .pdf format and transforms into .txt. Deletes files prior to 2012. Scrapes the .txt files and returns a CSV file named californiaresults.csv with "Week", "Week Ending", "State", "Season", "Total", "Lab", "Visits", and "Hospitalizations" data for each week. When the data is not available in the report, it returns N/A value. When the program was not able to parse the data, it returns a "could not retrieve" message. The "could not retrieve" values can then be collected manually.		 
+# Abstract:	Executes programs california.py and parsecalifornia.py. 
+#		Parses the file cali1.htm  -- which contains the HTML source for
+#		the page https://www.cdph.ca.gov/data/statistics/Pages/CISPDataArchive.aspx,
+#		and returns the URL of all weekly flu reports. Downloads the flu reports 
+#		in .pdf format and transforms into .txt. Deletes files prior to 2012. 
+#		Scrapes the .txt files and returns a CSV file named californiaresults.csv 
+#		with "Week", "Week Ending", "State", "Season", "Total", "Lab", "Visits", and 
+#		"Hospitalizations" data for each week. When the data is not available in the report, 
+#		it returns N/A value. When the program was not able to parse the data, it returns a 
+#		"could not retrieve" message. The "could not retrieve" values can then be collected manually.		 
 #------------------------------------------------------------------------------ 
-# Requirements: Python 2.7, 
+# Requirements: Python 2.7 
 #------------------------------------------------------------------------------ 
 # Notes: 
 #============================================================================== 
