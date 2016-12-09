79f2f9c22cba19164cb5e103853f66ae1e21f2f7
diasthalita <dias.t@husky.neu.edu>
2016-12-09 18:53:26 -0500

    Update parsecalifornia.py

--- california/parsecalifornia.py
+++ california/parsecalifornia.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 ##============================================================================== 
 # File:         parsecalifornia.py
 # Date:         Thu Dec  8 10:05:25 EST 2016
